63f633a75550873ada591755748eeaec5908165b
Stancu Florin <niflostancu@gmail.com>
2019-03-03 10:51:31 +0200

    switch to alacritty release (community) + update config

--- system/scripts/19-terminal.sh
+++ system/scripts/19-terminal.sh
@@ -5,13 +5,15 @@
 function do_install_prerequisites() {
     install_pkgs zsh tmux neovim python-neovim python2-neovim powerline-fonts xsel \
         the_silver_searcher screen minicom ruby fzf python-virtualenvwrapper xdotool \
-        ctags
+        ctags alacritty
 
-    install_aur_pkgs alacritty-git
+    # install_aur_pkgs alacritty-git
 }
 
 function do_configure() {
+    # Change ZSH config path globally to use XDG
     echo 'ZDOTDIR=$HOME/.config/zsh' > /etc/zsh/zshenv
+    # change root's shell
     chsh -s /usr/bin/zsh
 
     true
