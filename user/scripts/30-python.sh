7a3925dbca8ace5169e25f96daeefd0b3dff8e85
Stancu Florin <niflostancu@gmail.com>
2019-11-18 23:19:25 +0200

    user: more python linters

--- user/scripts/30-python.sh
+++ user/scripts/30-python.sh
@@ -2,7 +2,7 @@
 # 
 
 function do_configure() {
-    pip install --user pycodestyle pyflakes vim-vint proselint yamllint
+    pip install --user pycodestyle pyflakes vim-vint proselint yamllint pylint pylint-venv
     pip2 install --user pycodestyle vim-vint proselint yamllint
 }
 
