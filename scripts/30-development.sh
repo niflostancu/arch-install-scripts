92d171c350c8a5a96d298a21234a29c14883c59b
Stancu Florin <niflostancu@gmail.com>
2022-09-01 10:57:56 +0300

    development: update MCU udev rules + refactor packages
    (add some new tools)

--- scripts/30-development.sh
+++ scripts/30-development.sh
@@ -4,7 +4,10 @@
 
 function do_install_prerequisites() {
     # C / C++
-    install_pkgs base-devel gdb valgrind strace ltrace peda pwndbg cgdb bear
+    install_pkgs base-devel gdb valgrind strace ltrace peda pwndbg cgdb cmake bear
+
+    # Tools
+    install_pkgs lazygit
 
     # PHP
     install_pkgs php composer
@@ -12,37 +15,36 @@ function do_install_prerequisites() {
     # Golang
     install_pkgs go go-tools
 
-	# Java
-	install_pkgs maven scala gradle
-	install_pkgs jdk-openjdk
+    # Java
+    install_pkgs maven scala gradle
+    install_pkgs jdk-openjdk
 
-	# Python 3
-	install_pkgs python python-pip flake8 python-pylint python-pytest python-virtualenv
-	install_pkgs python-coverage cython pypy3 python-gobject pyenv
+    # Python 3
+    install_pkgs python python-pip flake8 python-pylint python-pytest python-virtualenv
+    install_pkgs python-coverage cython pypy3 python-gobject pyenv
 
-	# Python 2
-	install_pkgs python2 python2-pip python2-virtualenv
+    # Python 2
+    install_pkgs python2 python2-pip python2-virtualenv
 
-	# NodeJS
-	install_pkgs nodejs npm yarn
+    # NodeJS
+    install_pkgs nodejs npm yarn
 
-	# Embedded / uC
-	install_pkgs minicom cutecom avr-gcc avr-libc avrdude \
-		arm-none-eabi-gcc arm-none-eabi-gdb arm-none-eabi-newlib \
-		ninja ccache gperf cmake dfu-util
-	install_aur_pkgs openocd
-	# Sigrok for logic analyzers
-	install_pkgs pulseview
-	install_aur_pkgs openocd sigrok-cli sigrok-firmware-fx2lafw-bin 
+    # Embedded / uC
+    install_pkgs minicom picocom avr-gcc avr-libc avrdude openocd \
+        arm-none-eabi-gcc arm-none-eabi-gdb arm-none-eabi-newlib \
+        ninja ccache gperf dfu-util
+
+    # Sigrok for logic analyzers
+    install_pkgs pulseview sigrok-cli
 }
 
 # Copy configuration files to /etc 
 function do_configure() {
-	# Copy udev rules
-	if idem_rsync "$SRC_DIR/etc/udev/" /etc/udev/; then
-		udevadm control --reload-rules && sudo udevadm trigger
-	fi
+    # Copy udev rules
+    if idem_rsync "$SRC_DIR/etc/udev/" /etc/udev/; then
+        udevadm control --reload-rules && sudo udevadm trigger
+    fi
 
-	true
+    true
 }
 
