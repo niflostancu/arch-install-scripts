83265c39f94031b31fa9937accdcfe4b98abde64
Stancu Florin <niflostancu@gmail.com>
2023-09-07 14:17:12 +0300

    fonts: add missing / most popular fonts

--- scripts/10-fonts.sh
+++ scripts/10-fonts.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#
 # Installs & configures the font system
 
 function do_install_prerequisites() {
-    install_pkgs ttf-ubuntu-font-family ttf-dejavu ttf-sourcecodepro-nerd
+    install_pkgs gnu-free-fonts noto-fonts ttf-ubuntu-font-family ttf-dejavu \
+        ttf-liberation ttf-bitstream-vera ttf-sourcecodepro-nerd
+    install_pkgs --aur ttf-ms-win11-auto
 }
 
