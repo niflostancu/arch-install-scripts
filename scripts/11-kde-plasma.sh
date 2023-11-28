1088d1240dc043571cabf1d5eef22a97ae03be4a
Stancu Florin <niflostancu@gmail.com>
2023-11-28 11:54:04 +0200

    kde: only install specific multimedia apps

--- scripts/11-kde-plasma.sh
+++ scripts/11-kde-plasma.sh
@@ -3,12 +3,12 @@
 
 function do_install_prerequisites() {
     install_pkgs sddm plasma-meta plasma-nm kde-gtk-config packagekit-qt5 kmenuedit xsettingsd
-    install_pkgs kde-graphics-meta kde-multimedia-meta kde-network-meta kde-system-meta kde-utilities-meta
+    install_pkgs kde-multimedia-meta kde-network-meta kde-system-meta kde-utilities-meta
     # wayland!
     install_pkgs plasma-wayland-session egl-wayland wl-clipboard
     # Misc apps
     install_pkgs rsibreak redshift kwin-bismuth
-    install_pkgs --aur diskmonitor plasma5-applets-eventcalendar
+    install_pkgs --aur plasma5-applets-eventcalendar
 }
 
 function do_configure() {
--- scripts/20-multimedia-apps.sh
+++ scripts/20-multimedia-apps.sh
@@ -2,10 +2,10 @@
 # Installs Multimedia apps
 
 function do_install_prerequisites() {
-    install_pkgs vlc gstreamer gst-plugins-good \
-        gst-plugins-ugly phonon-qt5-gstreamer gst-libav \
-        phonon-qt5-vlc lame cdrtools cdrdao dvd+rw-tools k3b digikam \
-        cantata mpd pipewire pipewire-pulse
+    install_pkgs vlc gstreamer gst-plugins-good gst-plugins-ugly phonon-qt5-gstreamer gst-libav \
+        phonon-qt5-vlc lame cdrtools cdrdao dvd+rw-tools k3b digikam cantata mpd pipewire \
+        pipewire-pulse gwenview kamera kcolorchooser kdegraphics-thumbnailers \
+        kolourpaint kruler okular skanlite spectacle svgpart
     # Image manipulation
     install_pkgs gimp imagemagick inkscape dia
     # Music / video editing / conversion
