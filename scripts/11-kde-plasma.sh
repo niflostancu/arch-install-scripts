a38a49fad0087171f947d2301a49147010cb385c
Stancu Florin <niflostancu@gmail.com>
2024-04-12 19:13:42 +0300

    kde-plasma: add some missing util libs,
    - new kwin decoration plugin (active accent);
    - comment out event calendar (unsupported for Plasma 6)

--- scripts/11-kde-plasma.sh
+++ scripts/11-kde-plasma.sh
@@ -3,12 +3,14 @@
 
 function do_install_prerequisites() {
     install_pkgs sddm plasma-meta plasma-nm kde-gtk-config packagekit-qt5 kmenuedit xsettingsd
-    install_pkgs kde-multimedia-meta kde-network-meta kde-system-meta kde-utilities-meta
+    install_pkgs kde-multimedia-meta kde-network-meta kde-system-meta kde-utilities-meta \
+        kimageformats5 power-profiles-daemon kwallet-pam
     # wayland!
     install_pkgs plasma-wayland-session egl-wayland wl-clipboard
     # Misc apps
     install_pkgs rsibreak redshift kwin-bismuth
-    install_pkgs --aur plasma5-applets-eventcalendar
+    install_pkgs --aur plasma-active-accent-frame-decorations
+    #install_pkgs --aur plasma5-applets-eventcalendar
 }
 
 function do_configure() {
