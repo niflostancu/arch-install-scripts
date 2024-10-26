c66e86dc531b1aeef3690af4b09e484c80dc03f2
Stancu Florin <niflostancu@gmail.com>
2024-10-26 12:29:11 +0300

    packages: add linux-pbp, remove manjaro kernel

---
+++ packages/linux-pbp/build.conf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+
+NPROC=${NPROC:-$(nproc)}
+
+IFS='' read -r -d '' MAKEPKG_CONF_EXTRA <<EOF || true
+CARCH=aarch64
+MAKEFLAGS="-j$NPROC ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-"
+export CROSS_COMPILE=aarch64-linux-gnu-
+
+EOF
+
