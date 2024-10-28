432633fd7ada0e7dd2d43c2d59a4d782bb38bef1
Stancu Florin <niflostancu@gmail.com>
2024-10-28 21:01:34 +0200

    kernel: add kernel+microcode variant configs

--- scripts/00-boot-kernel.sh
+++ scripts/00-boot-kernel.sh
@@ -2,12 +2,14 @@
 # Kernel/boot-related packages & configs
 
 declare -a -g ADD_PLATFORM_PACKAGES
+INSTALL_KERNEL_VARIANT=${INSTALL_KERNEL_VARIANT:-linux}
+
 
 function do_install_prerequisites() {
-    install_pkgs linux-headers "${ADD_PLATFORM_PACKAGES[@]}"
+    install_pkgs "$INSTALL_KERNEL_VARIANT-headers" "${ADD_PLATFORM_PACKAGES[@]}"
 
-    if [[ -n "$INSTALL_X86_DRIVERS" ]]; then
-        install_pkgs intel-ucode
+    if [[ -n "$INSTALL_MICROCODE" ]]; then
+        install_pkgs "$INSTALL_X86_DRIVERS"-ucode
     fi
 }
 
