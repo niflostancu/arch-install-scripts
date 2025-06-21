9e2d292678da70cf27f7ae23bdab9e724c0e4040
Stancu Florin <niflostancu@gmail.com>
2025-06-21 21:05:39 +0300

    graphics: refactor nvidia to use pkg from var

--- scripts/10-xorg.sh
+++ scripts/10-xorg.sh
@@ -4,19 +4,26 @@
 
 
 function do_install_prerequisites() {
-    install_pkgs xorg-server xorg-apps xorg-xrandr xorg-xinput xbindkeys mesa \
+    install_pkgs xorg-server xorg-xrandr xorg-xinput xbindkeys mesa \
         mesa-demos xtrlock read-edid 
 
     if [[ -n "$INSTALL_NVIDIA_GRAPHICS" ]]; then
-        install_pkgs nvidia-dkms libglvnd nvidia-utils nvidia-settings \
+        local _NVPKG="nvidia-open-dkms"
+        if [[ ! "$INSTALL_NVIDIA_GRAPHICS" =~ ^[0-9]+$ ]]; then
+            _NVPKG="$INSTALL_NVIDIA_GRAPHICS"
+        fi
+        install_pkgs "$_NVPKG" libglvnd nvidia-utils nvidia-settings \
             lib32-nvidia-utils
+
+        if [[ "$INSTALL_GAMING_HYBRID" == "nvidia" ]]; then 
+            # bbswitch not required anymore!
+            # install_pkgs bbswitch-dkms
+            install_pkgs nvidia-prime
+        fi
     fi
 
     if [[ -n "$INSTALL_INTEL_GRAPHICS" ]]; then
         install_pkgs xf86-video-intel
     fi
-    # if [[ -n "$INSTALL_HYBRID_GRAPHICS" ]]; then
-    #     install_pkgs bumblebee primus
-    # fi
 }
 
--- scripts/40-gaming-3d.sh
+++ scripts/40-gaming-3d.sh
@@ -14,11 +14,6 @@ function do_install_prerequisites() {
     # Hybrid graphics
     install_pkgs vulkan-icd-loader lib32-vulkan-icd-loader vulkan-tools
 
-    if [[ "$INSTALL_GAMING_HYBRID" == "nvidia" ]]; then 
-        # bbswitch not required anymore!
-        # install_pkgs bbswitch-dkms
-        install_pkgs nvidia-prime
-    fi
     install_pkgs openbox
 }
 
