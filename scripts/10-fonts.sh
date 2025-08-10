1c06867497b039ad59389e2c35800ed4e9e147da
Stancu Florin <niflostancu@gmail.com>
2025-08-10 22:43:14 +0300

    fonts: add some more console/nerd fonts

--- scripts/10-fonts.sh
+++ scripts/10-fonts.sh
@@ -3,7 +3,9 @@
 
 function do_install_prerequisites() {
     install_pkgs gnu-free-fonts noto-fonts ttf-ubuntu-font-family ttf-dejavu \
-        ttf-liberation ttf-bitstream-vera ttf-sourcecodepro-nerd
+        ttf-liberation ttf-bitstream-vera ttf-sourcecodepro-nerd \
+        ttf-cascadia-code-nerd ttf-cascadia-mono-nerd \
+        ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono
     install_pkgs --aur ttf-ms-win11-auto
 }
 
