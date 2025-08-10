84f71022004784216ca5129de3fce46f4ffb5117
Stancu Florin <niflostancu@gmail.com>
2025-08-10 12:11:10 +0300

    multimedia: add all vlc plugins (they are separate now)

--- scripts/20-multimedia-apps.sh
+++ scripts/20-multimedia-apps.sh
@@ -2,7 +2,8 @@
 # Installs Multimedia apps
 
 function do_install_prerequisites() {
-    install_pkgs vlc gstreamer gst-plugins-good gst-plugins-ugly gst-libav \
+    install_pkgs vlc vlc-plugins-all
+    install_pkgs gstreamer gst-plugins-good gst-plugins-ugly gst-libav \
         lame cdrtools cdrdao dvd+rw-tools k3b digikam mpd pipewire \
         pipewire-pulse gwenview kamera kcolorchooser kdegraphics-thumbnailers \
         kolourpaint kruler okular skanlite spectacle svgpart
