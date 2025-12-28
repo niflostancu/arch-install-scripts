dbac5b8a4a6318f33afa72c3361b322f1b633e9c
Stancu Florin <niflostancu@gmail.com>
2025-12-28 11:33:16 +0200

    pacman: cleanup deprecated pacman.conf

--- scripts/01-base.sh
+++ scripts/01-base.sh
@@ -12,12 +12,6 @@ function do_configure() {
     systemctl enable sshd.service
     systemctl enable avahi-daemon
 
-    # Configure pacman (cachyos / multilib)
-    if [[ -n "$INSTALL_PACMAN_CONF" ]]; then
-        if idem_rsync_conf --opt -- "$INSTALL_PACMAN_CONF" /etc/pacman.conf; then
-            pacman --noconfirm -Syu
-        fi
-    fi
     # Sysctl config
     if idem_rsync_conf --all -- "sysctl.d/" /etc/sysctl.d/; then
         touch /etc/sysctl.conf
