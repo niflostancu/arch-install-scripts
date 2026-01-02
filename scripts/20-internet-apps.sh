5fbd42a46c189ba6981132d214ab8419fc378107
Stancu Florin <niflostancu@gmail.com>
2026-01-02 09:56:25 +0200

    internet-apps: add filezilla FTP client

--- scripts/20-internet-apps.sh
+++ scripts/20-internet-apps.sh
@@ -4,7 +4,7 @@
 
 function do_install_prerequisites() {
     install_pkgs chromium firefox transmission-qt transmission-cli
-    install_pkgs syncthing nextcloud-client
+    install_pkgs syncthing nextcloud-client filezilla
     # install_pkgs --aur signald libpurple-signald purple-gowhatsapp-git
     # Instant messaging
     install_pkgs discord signal-desktop neochat
@@ -13,14 +13,5 @@ function do_install_prerequisites() {
     # Email clients
     install_pkgs notmuch msmtp neomutt astroid protonmail-bridge-core offlineimap afew
     install_pkgs --aur dodo-mail-git lieer-git
-
-    _install_caprine
-}
-
-function _install_caprine() {
-    if check_pkg_installed caprine && [[ -z "$FORCE_REINSTALL" ]]; then
-        return 0
-    fi
-    build_custom_pkg -i --noconfirm caprine
 }
 
