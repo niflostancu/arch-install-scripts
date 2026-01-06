4ec05f2bd2799aa6dc8e93a88af7bf1945a1358e
Stancu Florin <niflostancu@gmail.com>
2026-01-06 14:30:50 +0200

    boot-kernel: renumber as 08, remove cachy sdboot hook

--- scripts/08-boot-kernel.sh
+++ scripts/08-boot-kernel.sh
@@ -4,8 +4,8 @@
 declare -a -g ADD_PLATFORM_PACKAGES
 INSTALL_KERNEL_VARIANT=${INSTALL_KERNEL_VARIANT:-linux}
 
-
 function do_install_prerequisites() {
+    pacman -Qi systemd-boot-manager &>/dev/null && pacman -R systemd-boot-manager || true
     install_pkgs "$INSTALL_KERNEL_VARIANT-headers" "${ADD_PLATFORM_PACKAGES[@]}"
 
     if [[ -n "$INSTALL_MICROCODE" ]]; then
