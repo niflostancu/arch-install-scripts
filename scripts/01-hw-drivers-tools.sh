0ab9928cc3aea9325da005be53bb2c7e54a4e385
Stancu Florin <niflostancu@gmail.com>
2026-02-22 10:54:42 +0200

    rename 01-disktools to 01-hw-drivers-tools
    + add fwupd

--- scripts/01-hw-drivers-tools.sh
+++ scripts/01-hw-drivers-tools.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-#
-# Installs disk / filesystem tools
+# Generic hardware drivers / tools
 
 function do_install_prerequisites() {
+    # disk utilities
     install_pkgs gptfdisk parted ntfs-3g smartmontools mtools \
         dosfstools xfsprogs btrfs-progs exfatprogs nvme-cli \
         lvm2 cryptsetup
-    # misc for advanced
+    # firmware tools
+    install_pkgs fwupd fwupd-efi
+    # security devices
     install_pkgs rng-tools
 }
 
